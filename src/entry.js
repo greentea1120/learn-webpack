d7866d15ea52bf13df8fba0262ecfe9ecb32969e
greentea <741024920@qq.com>
2017-11-16 10:31:01 +0800

    less&scss配置

--- src/entry.js
+++ src/entry.js
@@ -1,4 +1,6 @@
 import css from './css/index.css';
+import hello from './css/hello.less';
+import hello2 from './css/hello2.scss';
 // document.getElementById("title").innerHTML = "hello webpack";
 
 console.log('adddsdfsdfssssssssssdaa');
--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,28 @@ module.exports = {
             {
                 test:/\.html$/i,
                 use:['html-withimg-loader']
+            },
+            {
+                test:/\.less$/,
+                use: extractTextPlugin.extract({
+                    use:[{
+                        loader:"css-loader"
+                    },{
+                        loader:"less-loader"
+                    }],
+                    fallback:"style-loader"
+                })
+            },
+            {
+                test:/\.scss/,
+                use:extractTextPlugin.extract({
+                    use:[{
+                        loader:"css-loader"
+                    },{
+                        loader:"less-loader"
+                    }],
+                    fallback:"style-loader"
+                })
             }
         ]
     },
